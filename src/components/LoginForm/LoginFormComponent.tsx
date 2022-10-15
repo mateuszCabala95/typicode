6ab4a7d6d3488e4a9ca715405f78ad142dd97e0d
Mateusz Cabała <mateusz.cabala95@gmail.com>
2022-10-15 12:00:01 +0200

    Start work with Login page

--- src/components/LoginForm/LoginFormComponent.tsx
+++ src/components/LoginForm/LoginFormComponent.tsx
@@ -1,6 +1,20 @@
+import {useState} from "react";
+
 const LoginForm = () => {
+
+   const [loginData, setLoginData] = useState({
+       login: "",
+       password: ""
+   });
+
+   const handleSubmit = (e: any) => {
+       console.log(e);
+   }
     return (
-        <div>LoginForm</div>
+        <form onSubmit={handleSubmit}>
+
+            <button type="submit">Login</button>
+        </form>
     )
 }
 
